4f00a42c412ea2206f6b0bb7cb6eec8ea563e07d
yeralash <mishaneiman@gmail.com>
2024-05-15 01:22:36 -0400

    Made it sound more like a guitar

--- src/classes/Chord.js
+++ src/classes/Chord.js
@@ -17,9 +17,14 @@ export class Chord {
             'A': 9, 'A#': 10, 'Bb': 10,
             'B': 11
         };
-        const baseNote = noteMap[this.root];
-        // Adjust octave to ensure notes are in a hearable range (e.g., starting from the fourth octave)
-        const baseOctave = 4 * 12; // MIDI note number for the fourth octave
-        return this.type.intervals.map(interval => Tone.Frequency(baseOctave + baseNote + interval, 'midi').toNote());
+        
+        const stringTunings = ['E2', 'A2', 'D3', 'G3', 'B3', 'E4'];
+
+        const rootNote = noteMap[this.root];
+        return this.tab.map((fret, stringIndex) => {
+            if (fret === 'x') return null;
+            const stringNote = Tone.Frequency(stringTunings[stringIndex]).transpose(fret).toNote();
+            return stringNote;
+        }).filter(note => note !== null);
     }
 }
