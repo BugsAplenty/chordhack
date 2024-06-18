4bb675eb44357f15ccf3803e655e39e6fea43899
BugsAplenty <BugsAplenty@users.noreply.github.com>
2024-06-18 03:58:51 +0000

    deploy: 1737ce84ceb923b93af9420cc44bc7cbdac39417

--- src/utils/tunings.js
+++
@@ -1,8 +0,0 @@
-// tunings.js
-const tunings = {
-    standard: ['E2', 'A2', 'D3', 'G3', 'B3', 'E4'],
-    dropD: ['D2', 'A2', 'D3', 'G3', 'B3', 'E4'],
-    // Add more tunings as needed
-  };
-  
-  export default tunings;
